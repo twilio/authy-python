80035cb20d8b6d908f7cf69dc704c0f422636b5f
David Sanders <dsanders11@ucsbalum.com>
2017-07-16 21:28:14 -0700

    Add Python 3 to language classifiers in setup.py

--- setup.py
+++ setup.py
@@ -21,9 +21,11 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Security"
     ],
