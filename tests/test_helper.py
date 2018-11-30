ead3b003f5f1a491bc56f4474fbc092105983a0b
Kelley Robinson <krobinson@twilio.com>
2018-11-30 12:24:23 -0500

    re-add test_helper

---
+++ tests/test_helper.py
@@ -0,0 +1,16 @@
+API_KEY = "bf12974d70818a08199d17d5e2bae630"
+API_URL = "http://sandbox-api.authy.com"
+
+PHONE_NUMBER = '305-456-2345'
+COUNTRY_CODE = '1'
+API_USER_ID = 51  # real user id for this pn on the sandbox-api
+
+AUTH_ID_A = 14125
+AUTH_ID_B = ''
+
+NONCE = '1427849783.886085'
+URL = 'https://api.authy.com/dashboard/json/application/webhooks'
+PARAMS = {"b": "val|ue&2", "a": "value1"}
+
+POST_REQ_SIGNATURE = 'UJRjviTdXFA0xu4duxW9BWVDRZGsmSUDd4RglmzcX4o='
+GET_REQ_SIGNATURE = 'yjA/tsM6Rkxgww9wmZLxRWaouw21ZRsi7QIOdm7cEmA='
