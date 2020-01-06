ce7d3b4c984c0ef27e7e406c4d1f7e3d2d9bc8a2
Kelley Robinson <krobinson@twilio.com>
2020-01-06 17:00:28 -0500

    It's 2020, remove support for Python 2

--- setup.py
+++ setup.py
@@ -23,10 +23,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.5",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Security"
